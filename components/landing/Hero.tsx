af0f60df5fcfa226ab50a60188640e289ba13476
Dain Blodorn Kim <dain@db13.us>
2022-10-05 16:22:07 -0700

    linebreak

--- components/landing/Hero.tsx
+++ components/landing/Hero.tsx
@@ -3,7 +3,7 @@ import Readme from './../../README.md'
 export function Hero() {
   return (
     <section id="hero" className='pa--hero h-screen max-h-[80vmin] flex items-center'>
-      <article className='w-full max-w-[950px] m-auto'><Readme /></article>
+      <article className='w-full max-w-[884px] m-auto'><Readme /></article>
     </section>
   )
 }
