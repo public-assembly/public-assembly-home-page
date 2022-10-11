f1434d5cc2cd2fcf721f4d9bd2a21add18d8718c
Salief Lewis <salieflewis@gmail.com>
2022-10-11 16:06:49 -0500

    commented out notion temporarily

--- utils/constants.ts
+++ utils/constants.ts
@@ -2,12 +2,12 @@
  * Could be cool to have a utility to store all relevant Dao info on chain...
  */
 
-import { TWITTER_HANDLE } from "./env-vars"
+import { TWITTER_HANDLE } from './env-vars'
 
 export const entries = {
-  "twitter": `https://twitter.com/${TWITTER_HANDLE}`,
-  "github": `https://github.com/public-assembly`,
-  "more": `https://www.notion.so/ourzora/Public-Assembly-1243e6a5700f4d0587e63b644bff4197`
+  twitter: `https://twitter.com/${TWITTER_HANDLE}`,
+  github: `https://github.com/public-assembly`,
+  // "more": `https://www.notion.so/ourzora/Public-Assembly-1243e6a5700f4d0587e63b644bff4197`
 }
 
 /**
@@ -16,7 +16,7 @@ export const entries = {
 
 export const socials = Object.keys(entries).map((key) => {
   return {
-    'platform': key,
-    'url': entries[key as keyof typeof entries]
+    platform: key,
+    url: entries[key as keyof typeof entries],
   }
 })
