59359baab2337094f783c836cac399b0b7035b0e
tranqui.eth <thinkandchill11@gmail.com>
2022-12-01 17:06:57 -0500

    Revert "ecosystem, link list"

--- pages/ecosystem.tsx
+++ pages/ecosystem.tsx
@@ -1,34 +1,10 @@
 import type { NextPage } from 'next'
-import { Seo, ExternalLinkList, ExternalLink } from 'components'
-
-const ECOSYSTEM_LINKS = [
- {
-  title: 'PA Forum',
-  url: 'https://pblcasmbly.discourse.group/'
- },
- {
-  title: 'Dao Auction Site',
-  url: 'https://nouns.build'
- },
- {
-  title: 'Github',
-  url: 'https://github.com/public-assembly'
- },
- {
-  title: 'Docs',
-  url: 'https://public-assembly-docs.vercel.app/'
- },
- {
-  title: 'Twitter',
-  url: 'https://twitter.com/pblcasmbly'
- }
-] as ExternalLink[]
+import { Seo } from 'components'
 
 const Ecosystem: NextPage = () => {
   return (
     <>
       <Seo title='Ecosystem'/>
-      <ExternalLinkList links={ECOSYSTEM_LINKS}/>
     </>
   )
 }
