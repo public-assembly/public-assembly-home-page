11fe7b3f718a2c43d07f1c118a26789fce448a7b
dblodorn <dain@db13.us>
2022-12-01 10:07:40 -0500

    update header with new navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,14 @@
-import { Socials } from "./Socials"
-import { DisconnectButton } from "./elements"
+import dynamic from 'next/dynamic'
+import { Navigation } from './Navigation'
+
+const DisconnectButton = dynamic(() => import('./elements/DisconnectButton'), {
+  ssr: false,
+})
 
 export function Header() {
   return (
     <header className="flex flex-row justify-between items-center w-full px-6">
-      <div className="flex flex-row items-center gap-8">
-        <span className="font-bold">{process.env.NEXT_PUBLIC_SITE_TITLE}</span>
-        <Socials />
-      </div>
+      <Navigation />
       <DisconnectButton />
     </header>
   )
