2afba994148693878ac2e2ca34324356b13f63a0
dblodorn <dain@db13.us>
2022-12-02 11:53:19 -0800

    landing page data

--- data/nounishAuctions.ts
+++ data/nounishAuctions.ts
@@ -3,9 +3,29 @@ import gql from 'graphql-tag'
 /**
  * https://playground.api.zora.co/
  */
+export const TOKEN_METADATA_QUERY = gql`
+  query NounishAuctions($collectionAddress: String!, $tokenId: String!) {
+    token(
+      token: {address: $collectionAddress, tokenId: $tokenId}
+    ) {
+      token {
+        description
+        metadata
+      }
+    }
+  }
+`
 
 export const NOUNISH_AUCTIONS_QUERY = gql`
   query NounishAuctions($collectionAddress: String!, $network: NetworkInput) {
+    token(
+      token: {address: $collectionAddress, tokenId: "3"}
+    ) {
+      token {
+        description
+        metadata
+      }
+    }
     nouns {
       nounsActiveMarket(
         where: { collectionAddress: $collectionAddress }
